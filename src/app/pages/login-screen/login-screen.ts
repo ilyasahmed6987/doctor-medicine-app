b7eaabbc39f5db6494f814a30ed650c7be30feeb
ilyasahmed081 <ilyasahmed081@gmail.com>
2025-06-29 18:15:58 +0500

    working on reactive form and validation in signin and signup screens

--- src/app/pages/login-screen/login-screen.ts
+++ src/app/pages/login-screen/login-screen.ts
@@ -1,11 +1,35 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators, } from '@angular/forms';
 
 @Component({
   selector: 'app-login-screen',
-  imports: [],
+  imports: [ReactiveFormsModule, FormsModule, CommonModule],
   templateUrl: './login-screen.html',
-  styleUrl: './login-screen.scss'
+  styleUrl: './login-screen.scss',
 })
 export class LoginScreen {
+  loginForm: FormGroup;
+  submitted = false;
 
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      remember: [false],
+    });
+  }
+
+  get f() {
+    return this.loginForm.controls;
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    if (this.loginForm.valid) {
+      console.log('Success:', this.loginForm.value);
+    } else {
+      console.warn('Form is invalid');
+    }
+  }
 }
--- src/app/pages/signup-screen/signup-screen.ts
+++ src/app/pages/signup-screen/signup-screen.ts
@@ -1,11 +1,38 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators, } from '@angular/forms';
 
 @Component({
   selector: 'app-signup-screen',
-  imports: [],
+  imports: [ReactiveFormsModule, FormsModule, CommonModule],
   templateUrl: './signup-screen.html',
   styleUrl: './signup-screen.scss'
 })
 export class SignupScreen {
+
+  signupForm: FormGroup;
+  submitted = false;
+
+  constructor(private fb: FormBuilder) {
+    this.signupForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      agree: [false, Validators.requiredTrue]
+    });
+  }
+
+  get f() {
+    return this.signupForm.controls;
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    this.signupForm.markAllAsTouched();
+
+    if (this.signupForm.valid) {
+      console.log('Sign Up Successful!', this.signupForm.value);
+    }
+  }
 
 }
